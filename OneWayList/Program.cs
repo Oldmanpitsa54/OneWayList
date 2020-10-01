2bfde77b2d11738c8844cceda0657941ef3fcfe8
Oldmanpitsa54 <69981857+Oldmanpitsa54@users.noreply.github.com>
2020-10-01 17:36:19 +0300

    Add files via upload

---
+++ OneWayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace OneWayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cOneWayList list = new cOneWayList();
+            list.Add("pitsa");
+            list.Add("sushi");
+            list.Add("burrito");
+            list.Add("taco");
+            list.Add("ramen");
+
+            cOneWayList shitList = new cOneWayList();
+            shitList.Add("ramen");
+            shitList.Add("sushi");
+            
+            cOneWayList.Except(ref list, shitList);
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
